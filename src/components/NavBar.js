6c01b238ea4639afb9aeea385a9b1ee884a848f5
Hannah Glazier <hrayglazier@aol.com>
2022-02-23 11:35:35 -0700

    commit tree 17950d1b3b2b8b522c9e1ca0371d3bf3c71c5da7

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+    <a key="Home" href="#home">home</a>
+    <a key="About" href="#about">about</a>
+    <a key="ProjectList" href="#projects">projects</a>
+  </nav>;
 }
 
 export default NavBar;
